76745c36c8f40ada9ffb3ca83327bbf45ce13ed3
Bob Barthen <bobbarthen@gmail.com>
2021-12-25 15:41:56 -0600

    deleted:    src/utilities.cpp 	deleted:    src/utilities.h 	modified:   src/utils.cpp 	modified:   src/utils.h 	- moved utils namespace from utilities to utils

--- src/utils.h
+++ src/utils.h
@@ -11,6 +11,10 @@
 #include <json/json.h>
 #include <string>
 
+#include "stdio.h"
+#include <cstdarg>
+#include <vector>
+
 // don't use the UNUSED macro from StdString.h as it will generate "error: statement has no effect [-Werror=unused-value]"
 #define NOTUSED(x) ((void)x)
 
@@ -31,6 +35,23 @@ namespace BASE64
 {
 std::string b64_encode(unsigned char const* in, unsigned int in_len, bool urlEncode);
 }
+
+namespace Utils
+{
+
+std::vector<std::string> Split(const std::string& input,
+                               const std::string& delimiter,
+                               unsigned int iMaxStrings = 0);
+bool Str2Bool(const std::string& str);
+std::string Format(const char* fmt, ...);
+std::string FormatV(const char* fmt, va_list args);
+bool EndsWith(std::string const& fullString, std::string const& ending);
+bool StartsWith(std::string const& fullString, std::string const& starting);
+std::string GetDirectoryPath(std::string const& path);
+bool ReadFileContents(std::string const& strFileName, std::string& strResult);
+bool WriteFileContents(std::string const& strFileName, const std::string& strContent);
+
+} /* namespace Utils */
 
 std::string ToCIFS(std::string& UNCName);
 std::string ToUNC(std::string& CIFSName);
