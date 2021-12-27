ea5ef5861e09e24fbd972867d0ec8695245e06b8
rbarthen <bobbarthen@gmail.com>
2021-12-27 00:10:39 -0600

    test sorting

--- src/pvrclient-argustv.cpp
+++ src/pvrclient-argustv.cpp
@@ -770,6 +770,7 @@ PVR_ERROR cPVRClientArgusTV::GetRecordings(bool deleted,
               
               tag.SetTitle(displayTitle);
               /*tag.SetTitle(recording.Title());*/
+
               tag.SetEpisodeName(recording.Title());
               tag.SetPlotOutline(recording.SubTitle());
 
